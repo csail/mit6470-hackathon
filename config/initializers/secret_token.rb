86407efa4c7ce55c5ed132447fc1452792244e6c
Rachel Fong <rachefo@gmail.com>
2012-10-15 18:32:08 -0400

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Jampatch::Application.config.secret_token = '84b710b31f665f37d924750ad7d03e99ebbaa3f1c232f34aa8542b0f0c18d6089983fbc3320a611e51e406a036f3eb2eb9da303c1fb2fc8975a764d7a8e58e14'
