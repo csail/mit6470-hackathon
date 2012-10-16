2549d5098865ca4ac090adea40076362cfd32831
Rachel Fong <rachefo@gmail.com>
2012-10-15 23:26:45 -0400

    pg

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-Jampatch::Application.config.secret_token = '84b710b31f665f37d924750ad7d03e99ebbaa3f1c232f34aa8542b0f0c18d6089983fbc3320a611e51e406a036f3eb2eb9da303c1fb2fc8975a764d7a8e58e14'
+Jampatch::Application.config.secret_token = '2e092f96b4f929fac41156fb9fa61318f54c8732cd1c652d9c7b4e735ca7f2422650ff09071c7dad464d848d7fb76633180651a93b81361e230640033ef39f90'
