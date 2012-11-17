6f301891fe9320ba028fbcb44326432775274be4
Victor Costan <costan@gmail.com>
2012-11-17 13:59:58 -0500

    Scoreboard params: admin, category.

--- app/controllers/scores_controller.rb
+++ app/controllers/scores_controller.rb
@@ -1,12 +1,23 @@
 class ScoresController < ApplicationController
   before_filter :require_login
 
+  # GET /scores
+  # GET /scores?category=script
+  # GET /scores?admin=1
   def index
     bounce_user unless current_user.admin?
 
-    @users = User.all.reject! { |user| user.admin? }
+    @users = User.all
+    unless params[:admin]
+      @users.reject! { |user| user.admin? }
+    end
 
-    @problems = Problem.order(:name).all
+    if params[:category]
+      category = Category.all.find { |c| /#{params[:category]}/i =~ c.name }
+      @problems = category.problems.sort_by(&:name)
+    else
+      @problems = Problem.order(:name).all
+    end
     problem_submissions = {}
 
     @scores = {}
@@ -31,7 +42,9 @@ class ScoresController < ApplicationController
       @scores[user] = [ -score_sum, last_accepted_time ]
     end
 
-    @problems.reject! { |p| (problem_submissions[p] || 0) == 0 }
+    unless params[:category]
+      @problems.reject! { |p| (problem_submissions[p] || 0) == 0 }
+    end
 
     @users.sort_by! { |user| @scores[user] }
   end
