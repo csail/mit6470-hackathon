cedd77f99bb7792f6c761504c19c5a3b7cc9faf0
Victor Costan <costan@gmail.com>
2012-11-17 13:47:35 -0500

    Hide problems with zero submissions.

--- app/controllers/scores_controller.rb
+++ app/controllers/scores_controller.rb
@@ -7,6 +7,7 @@ class ScoresController < ApplicationController
     @users = User.all.reject! { |user| user.admin? }
 
     @problems = Problem.order(:name).all
+    problem_submissions = {}
 
     @scores = {}
     @submissions = {}
@@ -17,6 +18,7 @@ class ScoresController < ApplicationController
         submission = Submission.for_problem(problem).for_user(user).
                                 recent_first.first
         if submission
+          problem_submissions[problem] = (problem_submissions[problem] || 0) + 1
           @submissions[user][problem] = submission
           if submission.accepted?
             if last_accepted_time.to_f < submission.created_at.to_f
@@ -28,6 +30,8 @@ class ScoresController < ApplicationController
       score_sum = @submissions[user].values.map(&:score).sum
       @scores[user] = [ -score_sum, last_accepted_time ]
     end
+
+    @problems.reject! { |p| (problem_submissions[p] || 0) == 0 }
 
     @users.sort_by! { |user| @scores[user] }
   end
